da9635af676207f97e5306538aad6df0d2c6942a
kangsudal_notebook <kangsudal@gmail.com>
2022-08-24 16:29:41 +0900

    Add LinChart using fl_chart library

--- lib/widgets/bottomRight.dart
+++ lib/widgets/bottomRight.dart
@@ -1,4 +1,4 @@
-
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:performance_tracker/provider.dart';
 import 'package:provider/provider.dart';
@@ -8,6 +8,15 @@ class BottomRight extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<FlSpot> _lineData = [
+      // FlSpot(n, duration)
+      FlSpot(0, 0.0001),
+      FlSpot(500, 0.0001),
+      FlSpot(1000, 0.01),
+      FlSpot(1500, 5),
+      FlSpot(2000, 100),
+      FlSpot(2500, 50),
+    ];
     return Expanded(
       child: Container(
         color: Colors.white,
@@ -18,7 +27,7 @@ class BottomRight extends StatelessWidget {
             ),
             Expanded(
               child:
-              Text('duration:${context.watch<MyProviderModel>().duration}'),
+                  Text('duration:${context.watch<MyProviderModel>().duration}'),
             ),
             Expanded(
               child: Text(
@@ -34,6 +43,16 @@ class BottomRight extends StatelessWidget {
                     .watch<MyProviderModel>()
                     .addUpToSecondResultList
                     .toString(),
+              ),
+            ),
+            Expanded(
+              flex: 20,
+              child: LineChart(
+                LineChartData(
+                  lineBarsData: [
+                    LineChartBarData(spots: _lineData),
+                  ], // read about it in the LineChartData section
+                ),
               ),
             ),
           ],
